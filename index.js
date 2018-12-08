b8a16bbe9b155b4c01a6c7f5c48d5453e279daae
Elias Bennour <esieben1310@gmail.com>
2018-12-08 11:36:00 +0100

    Added multi language support

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const TeamspeakQuery = require('teamspeak-query');
 const readline = require('readline');
 var perms = require('./perms.json');
+var lang;
 var sq_host = "localhost";
 var sq_ts_port = 9987
 var sq_port = 10011;
@@ -14,72 +15,121 @@ const rl = readline.createInterface({
   output: process.stdout,
 }); 
 
-const question1 = () => {
-  return new Promise((resolve, reject) => {
-	  rl.question("Wie lautet dein Server Host? (Standard: localhost): ", function(answer) {
-		if(answer != "") {
-			sq_host = answer;
-		}
-		resolve();
-	  });
-  });
-}
-
-const question2 = () => {
+const ask = (question) => {
 	return new Promise((resolve, reject) => {
-		rl.question("Wie lautet dein Serverquery Port? (Standard: 10011): ", function(answer) {
-			if(answer != "") {
-				sq_port = answer;
-			}
-			resolve();
-		});
-	});
-}
-
-const question3 = () => {
-	return new Promise((resolve, reject) => {
-		rl.question("Wie lautet dein Serverquery Username? (Standard: serveradmin): ", function(answer) {
-			if(answer != "") {
-				sq_username = answer;
-			}
-			resolve();
-		});
-	});
-}
-
-const question4 = () => {
-	return new Promise((resolve, reject) => {
-		rl.question("Wie lautet dein Serverquery Passwort?: ", function(answer) {
-			if(answer != "") {
-				sq_pw = answer;
+		if(question == 0) {
+			rl.question("Please select a language\n1: Deutsch (German)\n2: English\n» ", function(answer) {
+				if(answer == 1) lang = 'de';
+				else lang = 'en';
 				resolve();
-			} else {
-				console.log("Dieses Feld darf nicht leer sein!");
-				question4();
+			});
+		} else if(lang == 'de') {
+			switch(question) {
+				case 1:
+					rl.question("Wie lautet dein Server Host? (Standard: localhost): ", function(answer) {
+						if(answer != "") {
+							sq_host = answer;
+						}
+						resolve();
+					});
+					break;
+				case 2:
+					rl.question("Wie lautet dein Serverquery Port? (Standard: 10011): ", function(answer) {
+						if(answer != "") {
+							sq_port = answer;
+						}
+						resolve();
+					});
+					break;
+				case 3:
+					rl.question("Wie lautet dein Serverquery Username? (Standard: serveradmin): ", function(answer) {
+						if(answer != "") {
+							sq_username = answer;
+						}
+						resolve();
+					});
+					break;
+				case 4:
+					rl.question("Wie lautet dein Serverquery Passwort?: ", function(answer) {
+						if(answer != "") {
+							sq_pw = answer;
+						} else {
+							sq_pw = "undefined";
+						}
+						resolve();
+					});
+					break;
+				case 5:
+					rl.question("Wie lautet dein Teamspeak Server Port? (Standard: 9987): ", function(answer) {
+						if(answer != "") {
+							sq_ts_port = answer;
+						}
+						resolve();
+					});
+					break;
+				case 6:
+					rl.question("Welchen Name soll die neue Gruppe haben? (Standard: Support++): ", function(answer) {
+						if(answer != "") {
+							groupname = answer;
+						}
+						resolve();
+					});
+					break;
 			}
-		});
-	});
-}
-
-const question5 = () => {
-	return new Promise((resolve, reject) => {
-		rl.question("Wie lautet dein Teamspeak Server Port? (Standard: 9987): ", function(answer) {
-			if(answer != "") {
-				sq_ts_port = answer;
+		} else if(lang == 'en') {
+			switch(question) {
+				case 1:
+					rl.question("What is your server host? (Default: localhost): ", function(answer) {
+						if(answer != "") {
+							sq_host = answer;
+						}
+						resolve();
+					});
+					break;
+				case 2:
+					rl.question("What is your Serverquery Port? (Default: 10011): ", function(answer) {
+						if(answer != "") {
+							sq_port = answer;
+						}
+						resolve();
+					});
+					break;
+				case 3:
+					rl.question("What is your Serverquery Username? (Default: serveradmin): ", function(answer) {
+						if(answer != "") {
+							sq_username = answer;
+						}
+						resolve();
+					});
+					break;
+				case 4:
+					rl.question("What is your Serverquery Password?: ", function(answer) {
+						if(answer != "") {
+							sq_pw = answer;
+						} else {
+							sq_pw = "undefined";
+						}
+						resolve();
+					});
+					break;
+				case 5:
+					rl.question("What is your Teamspeak Server Port? (Default: 9987): ", function(answer) {
+						if(answer != "") {
+							sq_ts_port = answer;
+						}
+						resolve();
+					});
+					break;
+				case 6:
+					rl.question("What name should the new group have? (Default: Support++): ", function(answer) {
+						if(answer != "") {
+							groupname = answer;
+						}
+						resolve();
+					});
+					break;
 			}
-			resolve();
-		});
-	});
-}
-
-const question6 = () => {
-	return new Promise((resolve, reject) => {
-		rl.question("Welchen Name soll die neue Gruppe haben? (Standard: Support++): ", function(answer) {
-			if(answer != "") {
-				groupname = answer;
-			}
-			resolve();
-		});
+		}
 	});
 }
 
@@ -120,26 +170,35 @@ function end() {
 
 const main = async () => {
 	console.log("##########################################################");
-	console.log("#       Willkommen beim Support++ Gruppen Generator      #");
+	console.log("#        Welcome to the Support++ group generator        #");
 	console.log("#                                                        #");
 	console.log("# Version: 1.0.0                                         #");
 	console.log("# Author: Elias Bennour                                  #");
-	console.log("# Bei Bugs/Fehlern bitte ein Issue auf Github erstellen. #");
+	console.log("# Any bugs or errors? Please create an Issue on Github.  #");
 	console.log("# Github: https://github.com/Support-pp/group-generator  #");
 	console.log("##########################################################");
-	await question1()
-	await question2()
-	await question3()
-	await question4()
-	await question5()
-	await question6()
+	await ask(0);
+	await ask(1);
+	await ask(2);
+	await ask(3);
+	await ask(4);
+	await ask(5);
+	await ask(6);
 	const query = new TeamspeakQuery.Raw({host:sq_host,port:sq_port});
 	await createGroup(query);
-	console.log("########################################");
-	console.log("# Deine Servergruppe wurde erstellt.   #");
-	console.log("# Die Rechte werden nun vergeben...    #");
-	console.log("# Das kann bis zu 2 Minuten dauern...  #");
-	console.log("########################################");
+	if(lang == 'de') {
+		console.log("########################################");
+		console.log("# Deine Servergruppe wurde erstellt.   #");
+		console.log("# Die Rechte werden nun vergeben...    #");
+		console.log("# Das kann bis zu 2 Minuten dauern...  #");
+		console.log("########################################");
+	} else {
+		console.log("########################################");
+		console.log("# Your server group has been created.  #");
+		console.log("# The rights are now awarded...        #");
+		console.log("# This can take up to 2 minutes...     #");
+		console.log("########################################");
+	}
 	setPerms(query);
 }
 
